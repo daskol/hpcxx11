7bd8f3fbb1043420cabb1815559b5de7ae083adb
Daniel Bershatsky <daniel.bershatsky@skolkovotech.ru>
2018-02-18 19:59:51 +0300

    Merge pull request #1.

--- src/hpc++11_omp.cc
+++ src/hpc++11_omp.cc
@@ -1,6 +1,7 @@
 #include "hpc++11.h"
 
 #include <cassert>
+#include <thread>
 #include <omp.h>
 
 void sum_vectors(std::vector<float> const &a,
@@ -10,15 +11,17 @@ void sum_vectors(std::vector<float> const &a,
     assert(a.size() == b.size());
     assert(a.size() == c.size());
 
+    unsigned nothreads = std::thread::hardware_concurrency();
+
     const auto a_data = a.data();
     const auto b_data = b.data();
     auto c_data = c.data();
     size_t size = c.size();
 
-    omp_set_num_threads(4);
+    omp_set_dynamic(0);
 
-#pragma omp parallel
-    for (size_t i = 0; i != size; ++i) {
+#pragma omp parallel for num_threads(nothreads) shared(size)
+    for (size_t i = 0; i < size; ++i) {
         c_data[i] = a_data[i] + b_data[i];
     }
 }
